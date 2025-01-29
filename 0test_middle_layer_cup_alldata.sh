7d7496a3fa8d0d38512f956848179c51fe809ed4
Shuang Li <shuangli59app@gmail.com>
2025-01-28 21:17:16 -0800

    Done comparing with DP

--- 0test_middle_layer_cup_alldata.sh
+++ 0test_middle_layer_cup_alldata.sh
@@ -1,10 +1,13 @@
 pkill -f eval_arx5
 
 
-# python scripts/eval_arx5.py \
-#     -i ../diffusion_policy/data/models/cup_wild_vit_l_1img.ckpt \
-#     -o data/experiments/0119_umi_cup_official \
-#     --no_mirror --policy_port 8769;
+python scripts/eval_arx5.py \
+    -i ../diffusion_policy/data/models/cup_wild_vit_l_1img.ckpt \
+    -o data/experiments/0128-cup_wild_vit_l_1img-eval \
+    --match_dataset_path data/experiments/0128-dataall_umi_cup-dataaugtype3-lr1e-5-2-bluecup-16diffstep-eval \
+    --match_episode 0 \
+    --task_name cup \
+    --no_mirror --policy_port 8769;
 
 # python scripts/eval_arx5.py \
 #     -i ../diffusion_policy/data/outputs_v4/umi_cup_all_data/dataall_umi_cup-dataaugtype3-lr1e-5/checkpoints/latest.ckpt \
@@ -15,10 +18,13 @@ pkill -f eval_arx5
 
 # python scripts/eval_arx5.py \
 #     -i ../diffusion_policy/data/outputs_v4/umi_cup_all_data/dataall_umi_cup-dataaugtype3-lr1e-5-2/checkpoints/latest.ckpt \
-#     -o data/experiments/0126-dataall_umi_cup-dataaugtype3-lr1e-5-2 \
+#     -o data/experiments/0128-dataall_umi_cup-dataaugtype3-lr1e-5-2-bluecup-16diffstep-eval \
 #     --different_history_freq \
 #     --policy_port 8768 --frequency 5 --steps_per_inference 4 \
-#     --task_name cup
+#     --task_name cup \
+    # --match_dataset_path data/experiments/0128-dataall_umi_cup-dataaugtype3-lr1e-5-2-bluecup-eval \
+    # --match_episode 0 \
+    
 
 # python scripts/eval_arx5.py \
 #     -i ../diffusion_policy/data/outputs_v4/umi/unified-act-autoregressive-cant-proj-newact-conv_fc-newdl-noshiftact-6actlayer-6imagelayer-repcur-normnone-diffhisfreq-no_mirror-diffinterval-newdl2-predprop-lr1e-5/checkpoints/latest.ckpt \
--- 0test_middle_layer_mix_500data.sh
+++ 0test_middle_layer_mix_500data.sh
@@ -65,19 +65,23 @@ pkill -f eval_arx5
 # DP
 python scripts/eval_arx5.py \
     -i ../diffusion_policy/data/outputs_v4/umi_fewdata_lang/0diffusion_policy_data500_umi_mix_-dataaug3-noactnorm/checkpoints/latest.ckpt \
-    -o data/experiments/0124_500data_dp \
+    -o data/experiments/0127-0diffusion_policy_data500_umi_mix_-dataaug3-noactnorm-80epoch-cup-eval \
     --policy_port 8769 \
     --frequency 5 --steps_per_inference 4 \
     --task_name cup \
-    --match_dataset_path data/experiments/0126-data500_umi_mix_dataaug-dataaugtype3-lr1e-5-5-cup-eval-final \
+    --match_dataset_path data/experiments/0126-data500_umi_mix_dataaug-dataaugtype3-lr1e-5-5-cup-eval \
     --match_episode 0
+
+    # --match_dataset_path data/experiments/0126-data500_umi_mix_dataaug-dataaugtype3-lr1e-5-5-towel-eval-final \
 
 # python scripts/eval_arx5.py \
 #     -i ../diffusion_policy/data/outputs_v4/umi_fewdata_lang/0diffusion_policy_data500_umi_mix_-dataaug3-noactnorm-lang/checkpoints/latest.ckpt \
-#     -o data/experiments/0124_500data_dp \
+#     -o data/experiments/0127-0diffusion_policy_data500_umi_mix_-dataaug3-noactnorm-lang-eval \
 #     --policy_port 8769 \
 #     --frequency 5 --steps_per_inference 4 \
-#     --task_name mouse
+#     --match_dataset_path data/experiments/0126-data500_umi_mix_dataaug-dataaugtype3-lr1e-5-5-cup-eval-final \
+#     --match_episode 0 \
+#     --task_name cup
 
 
 # Ours
@@ -91,10 +95,19 @@ python scripts/eval_arx5.py \
 
 # python scripts/eval_arx5.py \
 #     -i ../diffusion_policy/data/outputs_v4/umi_fewdata_lang/data500_umi_mix_dataaug-dataaugtype3-5-then-lr1e-5/checkpoints/latest.ckpt \
-#     -o data/experiments/0126-data500_umi_mix_dataaug-dataaugtype3-5-then-lr1e-5-cup-eval \
+#     -o data/experiments/0127-data500_umi_mix_dataaug-dataaugtype3-5-then-lr1e-5-cup-eval \
 #     --different_history_freq \
 #     --policy_port 8768 --frequency 5 --steps_per_inference 4 \
 #     --task_name cup
+
+
+# python scripts/eval_arx5.py \
+#     -i ../diffusion_policy/data/outputs_v4/umi_fewdata_lang/data500_umi_mix_dataaug-dataaugtype4-lr1e-5/checkpoints/latest.ckpt \
+#     -o data/experiments/0127-round2-data500_umi_mix_dataaug-dataaugtype4-lr1e-5-cup-eval-orangegripper \
+#     --different_history_freq \
+#     --policy_port 8768 --frequency 5 --steps_per_inference 4 \
+#     --task_name cup
+
 
 # python scripts/eval_arx5.py \
 #     -i ../diffusion_policy/data/outputs_v4/umi_fewdata_lang/data500_umi_mix_dataaug-dataaugtype3-lr1e-5-2/checkpoints/latest.ckpt \
